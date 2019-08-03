3ee3b671a9bb2094bdb5442fd852bfe6af0f4678
Ethan <19959944+c0ak@users.noreply.github.com>
2019-08-03 14:16:25 +0100

    Create signature.js

---
+++ signature.js
@@ -0,0 +1,9 @@
+// signature with javascript
+
+var gl = document.createElement('canvas').getContext('webgl') || document.createElement('canvas').getContext('experimental-webgl');
+var 米 = (((((((window.innerWidth || document.documentElement.clientWidth || document.documentElement.clientWidth)*(window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight))+(gl.getParameter(gl.getExtension('WEBGL_debug_renderer_info').UNMASKED_RENDERER_WEBGL))+(window.navigator.language + window.navigator.platform + window.navigator.appVersion)+(new Date().getTimezoneOffset())+window.navigator.deviceMemory+window.screen.colorDepth+window.sessionStorage.length+window.navigator.plugins.length).replace(/\s/g,'')).replace(/\(/g, "v")).replace(/_/g, 'x')).replace(/-/g, 'x')).replace(/\//g, 'x');
+var 世 = (((米.replace(/\)/g, 米.length)).replace(/\./g, 米.length)).replace(/,/g, 米.length)).replace(/;/g, 米.length).replace(/a/g, '一个').replace(/b/g, '是').replace(/c/g, '此').replace(/d/g, '的').replace(/e/g, '的').replace(/f/g, '在').replace(/g/g, '给').replace(/i/g, '世').replace(/j/g, '它是').replace(/k/g, '该').replace(/l/g, '升').replace(/m/g, '米').replace(/n/g, '不').replace(/o/g, '该').replace(/p/g, '上').replace(/q/g, '牛').replace(/r/g, '回覆').replace(/s/g, '小号').replace(/t/g, '至').replace(/u/g, '在').replace(/v/g, '和');
+
+console.log(世);
+
+// takes a few static, often varying variables given by the browser to identify a user.
